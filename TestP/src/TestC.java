23f89b457dfe200519149f440ace92e09857b157
Mohsen <Mohsen@JoonioMan>
2020-04-03 15:07:09 +0100

    new line added

--- TestP/src/TestC.java
+++ TestP/src/TestC.java
@@ -2,9 +2,9 @@
 public class TestC {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hi Guys");
 
+		System.out.println("Hi Guys");
+		System.out.println("How are you");
 	}
 
 }
