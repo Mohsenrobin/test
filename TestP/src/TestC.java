e7058262571938d49b9ff2eb88983229720b2110
Mohsenrobin <63107432+Mohsenrobin@users.noreply.github.com>
2020-04-03 15:39:16 +0100

    Update TestC.java

--- TestP/src/TestC.java
+++ TestP/src/TestC.java
@@ -6,12 +6,12 @@ public class TestC {
 		System.out.println("Hi Guys");
 		System.out.println("How are you");
 		System.out.println("W");
-		System.out.println("SSSS");
-		System.out.println("ddddd");
-		System.out.println("ddddd");
+		System.out.println("SSfdgSS");
+		System.out.println("ddddfgdgdd");
+		System.out.println("ddddfgdfgddd");
 				System.out.println("ddd11111dd");
 		System.out.println("ddddd");
-		System.out.println("ddddd");
+		System.out.println("dddfgdfgdgddd");
 
 	}
 
